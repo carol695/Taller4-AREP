8b6a39b143aed46798fac26c8344158921ced577
laboratorio laboratorio <laboratorio@labinfo.is.escuelaing.edu.co>
2023-02-17 14:38:52 -0500

    lab 4 clase

---
+++ src/main/java/org/example/Faa.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Faa {
+    @Test
+    public static void a1() { }
+    public static void a2() { }
+    @Test public static void a3() {
+        throw new RuntimeException("Boom");
+    }
+
+    public static void a4() { }
+    @Test public static void a5() { }
+    public static void a6() { }
+    @Test public static void a7() {
+        throw new RuntimeException("Crash");
+    }
+    public static void a8() { }
+}
